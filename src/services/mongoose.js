a96c12c179f48eef9047413b44ab370b116d754c
Metehan Akbaba <meitesantino@icloud.com>
2021-08-24 04:57:00 +0300

    code quality

--- src/services/mongoose.js
+++ src/services/mongoose.js
@@ -4,25 +4,10 @@ import { log } from 'console';
 
 mongoose.Promise = Promise;
 
-mongoose.connection.on('error', () => {
-  process.exit(-1);
-});
-
-mongoose.connection.on('reconnected', () => {
-  log('MongoDB Connection Reestablished');
-});
-
-mongoose.connection.on('disconnected', () => {
-  log('MongoDB Connection Disconnected');
-});
-
-mongoose.connection.on('close', () => {
-  log('MongoDB Connection Closed');
-});
-
 mongoose.connection.on('error', (error) => {
   log(`MongoDB Error: ${error}`);
 
   process.exit(1);
 });
+
 export default mongoose;
